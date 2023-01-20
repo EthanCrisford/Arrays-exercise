86f784d25266ede99cff440da98a397dd88c54fb
EthanCrisford <476052@chrome.escg.ac.uk>
2023-01-20 15:29:24 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+int[] myArray = new int[15];
+
+myArray[0] = 10;
+myArray[1] = 12;
+myArray[2] = 20;
+myArray[3] = 25;
+myArray[4] = 32;
+myArray[5] = 33;
+myArray[6] = 43;
+myArray[7] = 47;
+myArray[8] = 50;
+myArray[9] = 57;
+myArray[10] = 60;
+myArray[11] = 62;
+myArray[12] = 68;
+myArray[13] = 74;
+myArray[14] = 99;
+
+Console.WriteLine("The element at position 0 contains " + myArray[0]);
+
+if (myArray[0] == 10)
+{
+    Console.WriteLine("The element at position 1 contains " + myArray[1]);
+    Console.WriteLine("The element at position 2 contains " + myArray[2]);
+    Console.WriteLine("The element at position 3 contains " + myArray[3]);
+    Console.WriteLine("The element at position 4 contains " + myArray[4]);
+    Console.WriteLine("The element at position 5 contains " + myArray[5]);
+    Console.WriteLine("The element at position 6 contains " + myArray[6]);
+    Console.WriteLine("The element at position 7 contains " + myArray[7]);
+    Console.WriteLine("The element at position 8 contains " + myArray[8]);
+    Console.WriteLine("The element at position 9 contains " + myArray[9]);
+    Console.WriteLine("The element at position 10 contains " + myArray[10]);
+    Console.WriteLine("The element at position 11 contains " + myArray[11]);
+    Console.WriteLine("The element at position 12 contains " + myArray[12]);
+    Console.WriteLine("The element at position 13 contains " + myArray[13]);
+    Console.WriteLine("The element at position 14 contains " + myArray[14]);
+}
